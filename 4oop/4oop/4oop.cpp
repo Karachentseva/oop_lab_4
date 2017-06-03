7da2296572db9412288177da1079b00aa7129b94
Karachentseva <well.karachentseva@gmail.com>
2017-06-03 19:08:05 +0300

    oop_lab_4

---
+++ 4oop/4oop/4oop.cpp
@@ -0,0 +1,13 @@
+// 4oop.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include "fibonacci.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+system("pause");
+	return 0;
+}
+	
